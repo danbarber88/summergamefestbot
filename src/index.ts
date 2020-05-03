e8e9d9f60452cfd21803a715e10697a4650496b5
Dan Barber <d.j.barber88@gmail.com>
2020-05-03 13:49:34 +0100

    bot replying

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,26 @@
-console.log('Hello gamer');
+import * as Discord from "discord.js";
+import * as config from "./config.json";
+
+const client = new Discord.Client();
+
+client.on("ready", () => {
+    console.log("bot ready");
+});
+
+client.on("message", (message) => {
+    if (message.content.startsWith(config.prefix + "test")) {
+        message.channel.send("test success");
+    }
+
+    // Admin commands
+    if (message.author.id !== config.ownerID) {
+        // early return if any user but me tries to use command in the else.
+        return;
+    } else {
+        if (message.content.startsWith(config.prefix + "admin test")) {
+            message.channel.send("admin test success");
+        }
+    }
+});
+
+client.login(config.token);
