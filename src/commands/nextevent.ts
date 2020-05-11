0c78192d2459d8289df1b8c85e92082e10819445
Dan Barber <d.j.barber88@gmail.com>
2020-05-11 14:00:08 +0100

    switch to async/await

--- src/commands/nextevent.ts
+++ src/commands/nextevent.ts
@@ -2,43 +2,36 @@ import * as Discord from "discord.js";
 import Moment from "moment";
 import getNextEvent from "../helpers/getNextEvent";
 import whereToWatch from "../helpers/whereToWatch";
-import { IEvent } from '../IEvent'
 
-export const run = (
+export const run = async (
     _client: Discord.Client,
     message: Discord.Message,
     _args: String[]
 ) => {
-    getNextEvent()
-        .then((data: IEvent) => {
-            const embed = new Discord.MessageEmbed()
-                .setTitle(data.title)
-                .setDescription(data.description)
-                .setThumbnail(data.partner.logo)
-                .addField(
-                    "When",
-                    Moment(data.start)
-                        .utcOffset(-7)
-                        .format("MMMM Do, h:mm a [PT]") +
-                        "\n" +
-                        Moment(data.start)
-                            .utcOffset(-4)
-                            .format("MMMM Do, h:mm a [ET]") +
-                        "\n" +
-                        Moment(data.start)
-                            .utcOffset(+1)
-                            .format("MMMM Do, h:mm a [BST]")
-                )
-                .addField("Where", whereToWatch(data))
-                .setURL(data.url)
-                .setImage(data.image)
-                .setColor("#45b1ce");
+    const nextEvent = await getNextEvent();
 
-            message.channel.send({embed});
-        })
-        .catch(() => {
-            message.channel.send(
-                "Can't find next event right now. Try again later."
-            );
-        });
+    const embed = new Discord.MessageEmbed()
+        .setTitle(nextEvent.title)
+        .setDescription(nextEvent.description)
+        .setThumbnail(nextEvent.partner.logo)
+        .addField(
+            "When",
+            Moment(nextEvent.start)
+                .utcOffset(-7)
+                .format("MMMM Do, h:mm a [PT]") +
+                "\n" +
+                Moment(nextEvent.start)
+                    .utcOffset(-4)
+                    .format("MMMM Do, h:mm a [ET]") +
+                "\n" +
+                Moment(nextEvent.start)
+                    .utcOffset(+1)
+                    .format("MMMM Do, h:mm a [BST]")
+        )
+        .addField("Where", whereToWatch(nextEvent))
+        .setURL(nextEvent.url)
+        .setImage(nextEvent.image)
+        .setColor("#45b1ce");
+
+    message.channel.send({ embed });
 };
--- src/notifications/eventStart.ts
+++ src/notifications/eventStart.ts
@@ -2,47 +2,45 @@ import * as Discord from "discord.js";
 import Moment from "moment";
 import getNextEvent from "../helpers/getNextEvent";
 import whereToWatch from "../helpers/whereToWatch";
-import { IEvent } from "../IEvent";
 
 // TODO: set a timeout to delete the message after an hour?
 
-export default (client: Discord.Client) => {
+export default async (client: Discord.Client) => {
     const fiveMinsInMs = 1000 * 60 * 5;
+
+    const nextEvent = await getNextEvent();
 
     // Check if an event is starting every five minutes.
     setInterval(() => {
-        getNextEvent().then((data: IEvent) => {
-            console.log(`Checking for ${data.title} start`);
+        console.log(`Checking for ${nextEvent.title} start`);
 
-            // if five minutes from now is after the events start time.
-            if (Moment().add(5, "minutes") > Moment(data.start)) {
-                const embed = new Discord.MessageEmbed()
-                    .setTitle(
-                        `An event is scheduled to begin in less than 5 minutes.`
-                    )
-                    .setDescription(
-                        "Look below to find out where you can watch."
-                    )
-                    .setThumbnail(data.partner.logo)
-                    .addField(data.title, data.description)
-                    .addField("Watch", whereToWatch(data))
-                    .setURL(data.url)
-                    .setImage(data.image)
-                    .setColor("#45b1ce");
+        // if five minutes from now is after the events start time.
+        if (Moment().add(5, "minutes") > Moment(nextEvent.start)) {
+            const embed = new Discord.MessageEmbed()
+                .setTitle(
+                    `An event is scheduled to begin in less than 5 minutes.`
+                )
+                .setDescription("Look below to find out where you can watch.")
+                .setThumbnail(nextEvent.partner.logo)
+                .addField(nextEvent.title, nextEvent.description)
+                .addField("Watch", whereToWatch(nextEvent))
+                .setURL(nextEvent.url)
+                .setImage(nextEvent.image)
+                .setColor("#45b1ce");
 
-                const allGuilds = client.guilds.cache.array();
-                for (const guild of allGuilds) {
-                    const guildChannels = guild.channels.cache.array();
+            const allGuilds = client.guilds.cache.array();
+            for (const guild of allGuilds) {
+                const guildChannels = guild.channels.cache.array();
 
-                    for (const channel of guildChannels) {
-                        if (channel.type === "text") {
-                            (channel as Discord.TextChannel).send({
-                                embed,
-                            });
-                        }
+                for (const channel of guildChannels) {
+                    if (channel.type === "text") {
+                        (channel as Discord.TextChannel).send({
+                            embed,
+                        });
+                        break;
                     }
                 }
             }
-        });
+        }
     }, fiveMinsInMs);
 };
